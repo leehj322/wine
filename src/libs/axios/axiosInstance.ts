8e23d08861209354dad5ec907554af7ff64201f3
Jaeheon96 <97386020+Jaeheon96@users.noreply.github.com>
2024-08-31 03:34:31 +0900

    [TM-72] Feat(손재헌): 로그인 회원가입 프로토타입 (#4)
    * feat: auth prototype 구현
    
    * chore: 로그인 패스워드 input의 password 속성 삭제

--- src/libs/axios/axiosInstance.ts
+++ src/libs/axios/axiosInstance.ts
@@ -4,4 +4,15 @@ const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
+axiosInstance.interceptors.response.use(res => res, async (error) => {
+  const originalRequest = error.config;
+  if(error.response?.status === 401 && !originalRequest._retry) {
+    originalRequest._retry = true;
+    await axiosInstance.post("auth/refresh-token", null);
+    return axiosInstance(originalRequest);
+  }
+
+  return Promise.reject(error);
+})
+
 export default axiosInstance;
--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,3 +1,64 @@
+import { useRouter } from "next/router";
+import { ChangeEvent, FormEvent, useState } from "react";
+import axios from "@/libs/axios/axiosInstance";
+import { saveTokens } from "@/utils/authTokenStorage";
+
 export default function SignInPage() {
-  return <div>로그인 페이지</div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const router = useRouter();
+
+  const handleFormChange = (key: string, value: string) => {
+    setFormData((prev) => ({
+      ...prev,
+      [key]: value,
+    }));
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    handleFormChange(e.target.name, e.target.value);
+  };
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    let res;
+    try {
+      res = await axios.post("auth/signin", formData);
+    } catch {
+      alert(`Error: ${res?.status}`);
+      return;
+    }
+    const tokens = {
+      accessToken: res.data.accessToken,
+      refreshToken: res.data.refreshToken,
+    };
+    saveTokens(tokens);
+    router.push("/");
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>이메일</p>
+          <input
+            name="email"
+            className="border border-solid"
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          <p>비밀번호</p>
+          <input
+            name="password"
+            className="border border-solid"
+            onChange={handleInputChange}
+          />
+        </label>
+        <button type="submit">로그인</button>
+      </form>
+    </>
+  );
 }
