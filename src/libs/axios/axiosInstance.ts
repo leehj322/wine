af463bb8c3f0881c7d8a05aa842057e49938832f
Jaeheon96 <97386020+Jaeheon96@users.noreply.github.com>
2024-08-31 19:30:47 +0900

    [TM-64] Feat(손재헌): axios authorization 인터셉터와 갱신 인터셉터 구현 (#9)
    feat: axios request시 authorization 첨부하는 인터셉터와 토큰 만료시
    갱신하는 인터셉터 구현

--- src/libs/axios/axiosInstance.ts
+++ src/libs/axios/axiosInstance.ts
@@ -1,18 +1,39 @@
+import { removeTokens } from "@/utils/authTokenStorage";
 import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
-axiosInstance.interceptors.response.use(res => res, async (error) => {
-  const originalRequest = error.config;
-  if(error.response?.status === 401 && !originalRequest._retry) {
-    originalRequest._retry = true;
-    await axiosInstance.post("auth/refresh-token", null);
-    return axiosInstance(originalRequest);
-  }
+axiosInstance.interceptors.request.use((config) => {
+  const accessToken = localStorage.getItem("accessToken");
+  config.headers.Authorization = `bearer ${accessToken}`;
 
-  return Promise.reject(error);
-})
+  return config;
+});
+
+axiosInstance.interceptors.response.use(
+  (res) => res,
+  async (error) => {
+    const originalRequest = error.config;
+    const refreshToken = localStorage.getItem("refreshToken");
+    if (error.response?.status === 401 && refreshToken) {
+      let res;
+      try {
+        res = await axiosInstance.post("auth/refresh-token", {
+          refreshToken,
+        });
+      } catch {
+        removeTokens();
+        return Promise.reject(error);
+      }
+      const accessToken: string = res.data.accessToken;
+      localStorage.setItem("accessToekn", accessToken);
+      return axiosInstance(originalRequest);
+    }
+
+    return Promise.reject(error);
+  },
+);
 
 export default axiosInstance;
