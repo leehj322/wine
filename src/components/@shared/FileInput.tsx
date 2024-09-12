c8513a935281631cf83eb10761932772a7f79b0a
leehj322 <gudwns8956@naver.com>
2024-09-12 11:17:31 +0000

    refactor: 이미지 확장자 에러 처리

--- src/components/@shared/FileInput.tsx
+++ src/components/@shared/FileInput.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent, useState } from "react";
-import Image from "next/image";
+import NextImage from "next/image";
 
 interface FileInputProps {
   id: string;
@@ -24,7 +24,7 @@ export default function FileInput({
   const [previewImg, setPreviewImg] = useState<string | null>(initialImage);
 
   // image file extension checker
-  const imageExtensionCheck = (fileName: string) => {
+  const imageExtensionValidCheck = (fileName: string) => {
     const imageExtensions = ["jpg", "jpeg", "png", "bmp", "webp"];
     const extension = fileName.split(".").pop()?.toLowerCase();
 
@@ -33,8 +33,43 @@ export default function FileInput({
     return imageExtensions.includes(extension);
   };
 
+  // image file ratio checker
+  const imageRatioValidCheck = async (currentImgFile: File) => {
+    let isRatioValid = false;
+
+    const { width, height } = await new Promise<{
+      width: number;
+      height: number;
+    }>((resolve, reject) => {
+      const image = new Image();
+      const objectUrl = URL.createObjectURL(currentImgFile);
+      image.src = objectUrl;
+
+      image.onload = () => {
+        const imageSize = { width: image.width, height: image.height };
+        URL.revokeObjectURL(objectUrl);
+        resolve(imageSize);
+      };
+      image.onerror = () => {
+        URL.revokeObjectURL(objectUrl);
+        reject(new Error("image failed to load"));
+      };
+    });
+
+    if (width / height <= 0.3 && width / height >= 0.28) {
+      isRatioValid = true;
+    }
+
+    console.log("width: ", width);
+    console.log("height: ", height);
+    console.log("isRatioValid", isRatioValid);
+    console.log("----------------------------");
+
+    return isRatioValid;
+  };
+
   // file input change handler
-  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleFileChange = async (e: ChangeEvent<HTMLInputElement>) => {
     if (!hasPreview) return;
 
     // 파일 선택창을 눌렀다가 취소하는 경우에 선택된 파일을 전부 제거
@@ -48,7 +83,29 @@ export default function FileInput({
     if (e.target?.files) {
       const currentImgFile = e.target.files[0];
 
-      if (!currentImgFile || !imageExtensionCheck(currentImgFile.name)) return;
+      e.target.value = "";
+
+      if (!currentImgFile) return;
+
+      // 이미지 파일 확장자 검사
+      if (!imageExtensionValidCheck(currentImgFile.name)) {
+        alert("이미지 확장자는 jpg, jpeg, png, bmp, webp만 가능합니다!");
+        return;
+      }
+
+      // 이미지 파일 비율 검사
+      try {
+        const isRatioValid = await imageRatioValidCheck(currentImgFile);
+        if (!isRatioValid) {
+          alert(
+            "이미지 비율이 올바르지 않습니다. (권장 가로 : 세로 = 19 : 65)\n추천 사이즈 : 가로 119px, 세로 390px",
+          );
+          return;
+        }
+      } catch (error) {
+        console.error(error);
+        return;
+      }
 
       onChangeImage(currentImgFile);
       setPreviewImg(URL.createObjectURL(currentImgFile));
@@ -67,7 +124,7 @@ export default function FileInput({
       <div className="flex gap-3">
         <label htmlFor={id} className="cursor-pointer">
           <div className="flex h-[120px] w-[120px] items-center justify-center rounded-xl border-[1px] border-solid border-light-gray-300 md:h-[140px] md:w-[140px] md:rounded-2xl">
-            <Image
+            <NextImage
               width={32}
               height={32}
               src="/images/ic_camera.png"
@@ -77,9 +134,9 @@ export default function FileInput({
         </label>
         {hasPreview && previewImg && (
           <div className="relative h-[120px] w-[120px] md:h-[140px] md:w-[140px]">
-            <Image
+            <NextImage
               fill
-              className="rounded-xl object-cover"
+              className="rounded-xl object-contain"
               src={previewImg}
               alt="프리뷰 이미지"
             />
@@ -88,7 +145,7 @@ export default function FileInput({
               onClick={handleDelBtnClick}
               className="absolute right-1 top-1"
             >
-              <Image
+              <NextImage
                 width={20}
                 height={20}
                 src="images/icons/close.svg"
